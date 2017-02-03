e4b2eb6b2380cbff36bc5145a3999fd5d72e0e9a
Miguel Santisteban <mignan2010@outlook.com>
2017-02-03 19:06:14 -0200

    Update conexion.php

--- app/conexion.php
+++ app/conexion.php
@@ -1,8 +1,8 @@
 <?php
 $nombreservidor = "localhost";
 $nombreusuario = "root";
-$contrasenia = "mignan22";
-$basededatos = "trabajo_de_curso";
+$contrasenia = "*****";
+$basededatos = "nombreBaseDeDatos";
 
 // Creando la conexión
 $con = new mysqli($nombreservidor, $nombreusuario, $contrasenia, $basededatos);
@@ -10,20 +10,5 @@ $con = new mysqli($nombreservidor, $nombreusuario, $contrasenia, $basededatos);
 // Comprovando conexión
 if ($con->connect_error) {
 	die("Conexión Fallida: " . $con->connect_error);
-}
-
-
-//******************************************************************
-function saludo()
-{
-	echo "hola mundo";
-}
-//*************************************************************
-//Convierto los acentos a HTML
-function chao_tilde($entra)
-{
-$traduce=array( 'á' => '&aacute;' , 'é' => '&eacute;' , 'í' => '&iacute;' , 'ó' => '&oacute;' , 'ú' => '&uacute;' , 'ñ' => '&ntilde;' , 'Ñ' => '&Ntilde;' , 'ä' => '&auml;' , 'ë' => '&euml;' , 'ï' => '&iuml;' , 'ö' => '&ouml;' , 'ü' => '&uuml;');
-$sale=strtr( $entra , $traduce );
-return $sale;
 }
 ?>
